9fae1f5243830dd2e105b5bca1ccb6db857db33f
uqwecgt <artemcnp@gmail.com>
2023-09-06 00:34:46 +0300

    hw 1 done

---
+++ src/test/java/SearchTests.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+
+import static jdk.internal.misc.ThreadFlock.open;
+
+public class SearchTests {
+    @Test
+    void successfulSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://selenide.org"));
+    }
+}
